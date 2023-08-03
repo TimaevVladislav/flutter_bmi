6a4c638599829ff0744736c0b197c384d4b55240
timaevV <timaevvladislav@gmail.com>
2023-08-04 02:47:48 +0600

    add:final color

--- lib/widgets/card.dart
+++ lib/widgets/card.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 class CustomCard extends StatelessWidget {
-  const CustomCard({super.key});
+  CustomCard({required this.color});
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
     return Container(
         margin: EdgeInsets.all(15.0),
         decoration: BoxDecoration(
-            color: Color(0xFF1D1E33),
-            borderRadius: BorderRadius.circular(10.0)),
+            color: color, borderRadius: BorderRadius.circular(10.0)),
         height: 200.0,
         width: 170.0);
   }
