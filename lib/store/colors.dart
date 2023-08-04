99dc832e0e4d4e856518b9a40fed9fd22999ffc7
timaevV <timaevvladislav@gmail.com>
2023-08-05 02:10:57 +0600

    add:inactive color

--- lib/store/colors.dart
+++ lib/store/colors.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 
 const Color activeCard = Color(0xFF1D1E33);
+const Color inactiveCard = Color(0xFF111328);
 const Color bottomContainer = Color(0xFFEB1555);
