b5e1f25f068661d3cb117b0529ec8cb32c608f66
timaevV <timaevvladislav@gmail.com>
2023-08-05 15:42:20 +0600

    add:fix:title bold

--- lib/store/style.dart
+++ lib/store/style.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 
 const TextStyle counterTitle =
     TextStyle(fontSize: 50.0, fontWeight: FontWeight.w900);
-const TextStyle titles = TextStyle(fontSize: 18.0, color: Color(0xFF8D8E98));
+const TextStyle titles = TextStyle(
+    fontSize: 18.0, color: Color(0xFF8D8E98), fontWeight: FontWeight.bold);
