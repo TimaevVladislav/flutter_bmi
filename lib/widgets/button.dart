1f3837dd6ad44abaff529e055411d36a8d3f314e
timaevV <timaevvladislav@gmail.com>
2023-08-05 15:57:14 +0600

    add:state change

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -1,15 +1,18 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 
 class RoundActionButton extends StatelessWidget {
-  RoundActionButton({required this.icon});
+  RoundActionButton({required this.icon, required this.pressed});
   final Icon icon;
+  final pressed;
 
   @override
   Widget build(BuildContext context) {
     return RawMaterialButton(
-        onPressed: () {},
+        onPressed: pressed,
         shape: CircleBorder(),
-        elevation: 6.0,
+        elevation: 0.0,
         constraints: BoxConstraints.tightFor(width: 56.0, height: 56.0),
         fillColor: Color(0xFF4C4F5E),
         child: icon);
