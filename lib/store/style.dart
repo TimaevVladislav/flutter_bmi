266dc772f93b13511fb6aeaec97977df96fe7416
timaevV <timaevvladislav@gmail.com>
2023-08-05 13:53:58 +0600

    add:global style

---
+++ lib/store/style.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const TextStyle counterTitle =
+    TextStyle(fontSize: 50.0, fontWeight: FontWeight.w900);
+const TextStyle titles = TextStyle(fontSize: 18.0, color: Color(0xFF8D8E98));
