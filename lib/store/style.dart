91f05f8ba18b1729687d9bf6c5cd04ca69230a0e
timaevV <timaevvladislav@gmail.com>
2023-08-06 13:37:29 +0600

    add:answer bmi

--- lib/store/style.dart
+++ lib/store/style.dart
@@ -6,3 +6,8 @@ const TextStyle titles = TextStyle(
     fontSize: 18.0, color: Color(0xFF8D8E98), fontWeight: FontWeight.bold);
 const TextStyle calculateTitle =
     TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold);
+const TextStyle resultTitle = TextStyle(
+    color: Color(0xFF24D876), fontWeight: FontWeight.bold, fontSize: 22.0);
+const TextStyle bmiTitle =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 100.0);
+const TextStyle answerBmiTitle = TextStyle(fontSize: 22.0);
