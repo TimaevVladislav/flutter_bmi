c631b00688045b0e17dc000758ecda5aae0b753c
timaevV <timaevvladislav@gmail.com>
2023-08-06 13:01:09 +0600

    add:results screen

---
+++ lib/screens/results.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Center(child: Text('BMI CALCULATOR'))));
+  }
+}
