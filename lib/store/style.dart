5bdffc5da8ad469c8f77f9261594128abddcc743
timaevV <timaevvladislav@gmail.com>
2023-08-06 13:00:32 +0600

    add:style calculate

--- lib/store/style.dart
+++ lib/store/style.dart
@@ -4,3 +4,5 @@ const TextStyle counterTitle =
     TextStyle(fontSize: 50.0, fontWeight: FontWeight.w900);
 const TextStyle titles = TextStyle(
     fontSize: 18.0, color: Color(0xFF8D8E98), fontWeight: FontWeight.bold);
+const TextStyle calculateTitle =
+    TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold);
